14b89623ad9b42382eadbfaa64db44f96a95c5b6
Tambua Evaristus <evaristustambua@gmail.com>
2022-11-02 11:16:56 +0100

    Project initialized

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+useEffect(()=>{
+
+
+    window.$ = window.jQuery = require('jquery')
+
+    import("bootstrap/dist/js/bootstrap");
+
+    $(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
+});
+
+$(".nav-link").on("click", function(){
+	$(".nav-link.active-link").removeClass("active-link");
+	$(this).addClass("active-link");
+});
+
+
+},[])
   return <Component {...pageProps} />
 }
 
